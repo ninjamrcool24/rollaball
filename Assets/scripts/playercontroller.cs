497239a1eaf8fd864cda7c72152f2a4d402cdef6
UCode <ucode@UCode-MBA13-T03.fios-router.home>
2017-12-20 18:08:13 -0800

    build game

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class playercontroller : MonoBehaviour {
-
+public class playercontroller : MonoBehaviour
+{
     public Rigidbody rb;
     public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
     }
     
     void FixedUpdate()
@@ -19,5 +26,20 @@ public class playercontroller : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pick Up")){
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+            countText.text = "Count : " + count.ToString();
+            if( count >= 9){
+                winText.text = "You Win!";
+            }
     }
 }
